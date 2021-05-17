31670cb5ee1ede09125f1d2c4bcd2cc3c5c1896e
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-05-17 00:51:19 -0400

    oooh routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
-
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
@@ -10,6 +10,17 @@ export default function Home() {
       </Head>
 
       <h1>Hello World!</h1>
+      <ArticleList articles={articles}/>
     </div>
   )
+}
+
+export const getStaticProps = async () =>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const articles = await res.json()
+  return {
+    props:{
+      articles
+    }
+  }
 }
